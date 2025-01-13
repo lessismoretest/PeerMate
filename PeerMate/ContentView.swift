f02787541bc5591e0002804824560af99ba1bf58
Pokkepokke <53515721+lessismoretest@users.noreply.github.com>
2025-01-14 00:21:31 +0800

    Initial Commit

---
+++ PeerMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeerMate
+//
+//  Created by Less is more on 2025/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeerMate/PeerMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerMateApp.swift
+//  PeerMate
+//
+//  Created by Less is more on 2025/1/14.
+//
+
+import SwiftUI
+
+@main
+struct PeerMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
